439df2771ace2dfb16bf55b1047c0413ebca2224
leahzhangl <52469424+leahzhangl@users.noreply.github.com>
2024-10-27 01:00:11 -0700

    Update test.py

--- test.py
+++ test.py
@@ -11,15 +11,14 @@ def main():
 
 #Then the program asks the user to provide N numbers (one by one) and reads all of them (again, one by one)
 	numbers = []
-
- 	for i in range(N):
-        	num = int(input(f"Enter number {i + 1}: "))
+	
+	for i in range(N):
+		num = int(input(f"Enter number {i + 1}: "))
 		print(f"{num}")
-        	numbers.append(num)
+		numbers.append(num)
 
 #In the end, the program asks the user for input X (integer) and outputs: "-1" if there were no such X among N read numbers, 
 #or the index (from 1 to N) of this X if the user inputed it before.
-
 	X = int(input("Enter the integer X to search for: "))
 	
 	if X in numbers:
