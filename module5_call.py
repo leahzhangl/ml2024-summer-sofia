6e5efaccecd5e66a8607c0b732dd0c1b2d22abe6
leahzhangl <52469424+leahzhangl@users.noreply.github.com>
2024-10-31 16:39:50 -0700

    Update module5_call.py

--- module5_call.py
+++ module5_call.py
@@ -6,3 +6,15 @@
 #i.e., create the module with the Class described in the item above, and name this module "module5_mod.py" + 
 #create the main program "module5_call.py" that uses the Class description from the "*_mod" file.
 
+from module5_mod import Integer
+
+def main():
+	N = int(input("Please input a positive integer N: "))
+	M = Integer()
+	M.inputnumbers(N)
+	
+	X = int(input("Enter the integer X to search for: "))
+	M.findindex(X)
+
+if __name__ == "__main__":
+	main()
