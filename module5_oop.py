eeae12e69cbd67d3100e5aa33815ad4e1fa9d7f3
leahzhangl <52469424+leahzhangl@users.noreply.github.com>
2024-10-31 15:09:10 -0700

    Update module5_oop.py

--- module5_oop.py
+++ module5_oop.py
@@ -8,18 +8,18 @@
 #In the end, the program asks the user for input X (integer) and outputs: "-1" if there were no such X among N read numbers, or the index (from 1 to N) of this X if the user inputted it before.
 
 class Integer():
-    def main(self, N, X):
-        self.N = int(input("Please input a positive integer N: "))
-        numbers = []
-        for i in range(N):
-	  num = int(input(f"Enter number {i + 1}: "))
-          numbers.append(num)
-        self.X = int(input("Enter the integer X to search for: "))
-        if X in numbers:
-	  index = numbers.index(X) + 1\
-          print(f"{index}")
-        else:
-          print("-1")
+	def main(self, N, X):
+		self.N = int(input("Please input a positive integer N: "))
+		numbers = []
+		for i in range(N):
+			num = int(input(f"Enter number {i + 1}: "))
+			numbers.append(num)
+		self.X = int(input("Enter the integer X to search for: "))
+		if X in numbers:
+			index = numbers.index(X) + 1\
+			print(f"{index}")
+		else:
+			print("-1")
 
 jackman = Integer()
 jackman.N("4")
