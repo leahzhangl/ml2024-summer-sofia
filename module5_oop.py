a4b59cd0fbba309d0501ed8ef5ba4898fe3d97f6
leahzhangl <52469424+leahzhangl@users.noreply.github.com>
2024-10-31 14:13:01 -0700

    Create module5_oop.py

---
+++ module5_oop.py
@@ -0,0 +1,12 @@
+### MSCS2202
+### Yuxin Zhang
+### HW5.2
+
+#The basic functionality of data processing (data initialization, data insertion, data search) should be done via Object-Oriented Programming Paradigm (i.e. using Classes)
+#The program asks the user for input N (positive integer) and reads it
+
+
+#Then the program asks the user to provide N numbers (one by one) and reads all of them (again, one by one)
+
+#In the end, the program asks the user for input X (integer) and outputs: "-1" if there were no such X among N read numbers, or the index (from 1 to N) of this X if the user inputted it before.
+
