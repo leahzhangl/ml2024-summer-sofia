585073497d7b4181e086a32d86ba31066a2b05dd
leahzhangl <52469424+leahzhangl@users.noreply.github.com>
2024-10-31 14:50:27 -0700

    Update module5_call.py

--- module5_call.py
+++ module5_call.py
@@ -1,3 +1,8 @@
 ### MSCS2202
 ### Yuxin Zhang
 ### HW5.2
+
+#Create the functionality outside of the main running code: 
+#i.e., create the module with the Class described in the item above, and name this module "module5_mod.py" + 
+#create the main program "module5_call.py" that uses the Class description from the "*_mod" file.
+
