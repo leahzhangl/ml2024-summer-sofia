2ef08df08a5cd4b331cf7e58c3cd311d3a5181ae
leahzhangl <52469424+leahzhangl@users.noreply.github.com>
2024-11-17 22:16:23 -0800

    Update module7_knn-regr-scikit.py

--- module7_knn-regr-scikit.py
+++ module7_knn-regr-scikit.py
@@ -2,3 +2,18 @@
 ### HW7.2
 ### Yuxin Zhang
 
+#The program asks the user for input N (positive integer) and reads it.
+#Then the program asks the user for input k (positive integer) and reads it.
+#Then the program asks the user to provide N (x, y) points (one by one) and reads all of them: 
+#first: x value, then: y value for every point one by one. X and Y are the real numbers.
+#In the end, the program asks the user for input X and outputs: the result (Y) of k-NN Regression if k <= N, or any error message otherwise.
+#Additionally, provide the variance of labels in the training dataset.
+#The basic functionality of data processing (data initialization, data insertion), 
+#should be done using Numpy library while the computation (ML) part should be done using Scikit-learn library as much as possible 
+#(note: you can combine with what you've done from the previous task).
+
+import numpy as np
+from sklearn.neighbors import KNeighborsRegressor
+
+
+
