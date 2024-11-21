90611dab04cc389aabf8c7deb9253c9eadf02dea
leahzhangl <52469424+leahzhangl@users.noreply.github.com>
2024-11-21 10:03:23 -0800

    Update module8_metrics-scikit.py

--- module8_metrics-scikit.py
+++ module8_metrics-scikit.py
@@ -11,19 +11,23 @@
 import numpy as np
 from sklearn.metrics import precision_score, recall_score
 
-N = int(input("How many data points would you set: "))
+def main():
+	N = int(input("How many data points would you set: "))
 		
-X = np.zeros(N)
-Y = np.zeros(N)
+	X = np.zeros(N)
+	Y = np.zeros(N)
 	
-for i in range(N):
-	X[i] = int(input(f"Enter either 0 or 1 for X (correct) values for point {i+1}: "))
-	Y[i] = int(input(f"Enter either 0 or 1 for Y (predicted) values for point {i+1}: "))
+	for i in range(N):
+		X[i] = int(input(f"Enter either 0 or 1 for X (correct) values for point {i+1}: "))
+		Y[i] = int(input(f"Enter either 0 or 1 for Y (predicted) values for point {i+1}: "))
 
-precision = precision_score(X, Y)
-recall = recall_score(X, Y)
+	precision = precision_score(X, Y)
+	recall = recall_score(X, Y)
 
-print(f"Precision: {precision:.2f}")
-print(f"Recall: {recall:.2f}")
+	print(f"Precision: {precision:.2f}")
+	print(f"Recall: {recall:.2f}")
+
+if __name__ == "__main__":
+	main()
 
 
