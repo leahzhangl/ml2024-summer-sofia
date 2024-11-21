7acab1603e8b53a84ba65f56945c461afc25d972
leahzhangl <52469424+leahzhangl@users.noreply.github.com>
2024-11-21 09:27:40 -0800

    Update module8_metrics-scikit.py

--- module8_metrics-scikit.py
+++ module8_metrics-scikit.py
@@ -8,3 +8,38 @@
 ###should be done using Numpy library while the computation (ML) part should be done using Scikit-learn library as much as possible 
 ###(note: you can combine with what you've done from the previous tasks).
 
+import numpy as np
+from sklearn.neighbors import KNeighborsRegressor
+
+def KNNregression(X, y, X_test, k):
+	
+	model = KNeighborsRegressor(n_neighbors=k)
+	model.fit(X, y)
+	Y_pred = model.predict([X_test])
+	variance = np.var(y)
+	
+	return Y_pred[0], variance
+
+
+def main():
+	N = int(input("How many data points would you set: "))
+	k = int(input("Which level of kNN would you use(number of neighbors): "))
+	if k > N:
+		return "Error: k should be less than or equal to N!"
+			
+	X = np.zeros((N, 2))
+	y = np.zeros(N)
+	
+	for i in range(N):
+		x, y[i] = map(float, input(f"Enter x and y values for point {i+1}: ").split())
+		X[i] = [x, y[i]]
+	
+
+	X_test = list(map(float, input("Enter the test value X to predict its correlative Y: "))
+	Y_pred, variance = KNNregression(X, y, X_test, k)
+
+	print(f"The Y is  {Y_pred} ")
+	print(f"The variance is {variance} ")
+	
+if __name__ == "__main__":
+	main()
